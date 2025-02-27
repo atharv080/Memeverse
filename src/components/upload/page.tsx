f48d2b4a3af0497e0bb14e2cfcabba09dbf85517
Atharv <atharvsinghal421@gmail.com>
2025-02-28 01:45:54 +0530

    fix upload 3

--- src/components/upload/page.tsx
+++ src/components/upload/page.tsx
@@ -99,6 +99,8 @@ export default function UploadPage() {
               onCaptionChange={handleCaptionChange}
               onGenerateAI={generateAICaption}
               isGenerating={isGenerating}
+              topText={topText}       // Add this
+              bottomText={bottomText} // Add this
             />
           )}
         </div>
